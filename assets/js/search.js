a4c2978c5b33c8d2a9230879f19cf389d2d413b7
bikash119 <bikash119@users.noreply.github.com>
2021-08-18 19:28:21 +0000

    deploy: fd4928ff27e39b64102a23df3c69dcc243065ece

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/fastai-questionaire/assets/js/search-data.json', true);
+    request.open('GET', '/fastbook/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
